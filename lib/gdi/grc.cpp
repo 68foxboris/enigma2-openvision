f5aa2d876219ea8cd95bc14c4036d04c12d9f920
Boris <boris.fox.solovyev@gmail.com>
2025-11-30 06:06:58 +0200

    Fix error: invalid conversion from 'const char8_t*' to 'char' [-fpermissive]

--- lib/gdi/grc.cpp
+++ lib/gdi/grc.cpp
@@ -798,7 +798,7 @@ void gDC::exec(const gOpcode *o)
 			if (flags & gPainter::RT_WRAP) // Remove wrap
 				flags -= gPainter::RT_WRAP;
 			std::string text = o->parm.renderText->text;
-			text += u8"…";
+			text += (const char *)u8"…";
 
 			eTextPara testpara(o->parm.renderText->area);
 			testpara.setFont(m_current_font);
