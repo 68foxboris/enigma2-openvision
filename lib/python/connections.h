260f52c5fc1ff4d8228514f9765e4c89fd3919c8
Boris <boris.fox.solovyev@gmail.com>
2025-12-01 22:09:29 +0200

    Fix error: 'PyInt_FromLong' was not declared in this scope

--- lib/python/connections.h
+++ lib/python/connections.h
@@ -6,8 +6,6 @@
 #include <lib/python/python.h>
 #include <utility>
 
-#include <Python.h>
-
 class PSignal
 {
 protected:
@@ -24,29 +22,21 @@ public:
 
 inline PyObject *PyFrom(int v)
 {
-	return PyInt_FromLong(v);
+	return PyLong_FromLong(v);
 }
 
 inline PyObject *PyFrom(const char *c)
 {
-	return PyString_FromString(c);
+	return PyUnicode_FromString(c);
 }
-// Only used by console which might transfer binary data(screenshots). So use Bytes instead of Unicode
+
 inline PyObject *PyFrom(std::pair<const char*, int>& p)
 {
-#if PY_MAJOR_VERSION < 3
-	return PyString_FromStringAndSize(p.first, p.second);
-#else
 	return PyBytes_FromStringAndSize(p.first, p.second);
-#endif
 }
 
 template <class R>
-#if SIGCXX_MAJOR_VERSION == 3
 class PSignal0: public PSignal, public sigc::signal<R()>
-#else
-class PSignal0: public PSignal, public sigc::signal0<R>
-#endif
 {
 public:
 	R operator()()
@@ -57,20 +47,12 @@ public:
 			callPython(pArgs);
 			Org_Py_DECREF(pArgs);
 		}
-#if SIGCXX_MAJOR_VERSION == 3
 		return sigc::signal<R()>::operator()();
-#else
-		return sigc::signal0<R>::operator()();
-#endif
 	}
 };
 
 template <class R, class V0>
-#if SIGCXX_MAJOR_VERSION == 3
 class PSignal1: public PSignal, public sigc::signal<R(V0)>
-#else
-class PSignal1: public PSignal, public sigc::signal1<R,V0>
-#endif
 {
 public:
 	R operator()(V0 a0)
@@ -82,20 +64,12 @@ public:
 			callPython(pArgs);
 			Org_Py_DECREF(pArgs);
 		}
-#if SIGCXX_MAJOR_VERSION == 3
 		return sigc::signal<R(V0)>::operator()(a0);
-#else
-		return sigc::signal1<R,V0>::operator()(a0);
-#endif
 	}
 };
 
 template <class R, class V0, class V1>
-#if SIGCXX_MAJOR_VERSION == 3
 class PSignal2: public PSignal, public sigc::signal<R(V0, V1)>
-#else
-class PSignal2: public PSignal, public sigc::signal2<R,V0,V1>
-#endif
 {
 public:
 	R operator()(V0 a0, V1 a1)
@@ -108,20 +82,12 @@ public:
 			callPython(pArgs);
 			Org_Py_DECREF(pArgs);
 		}
-#if SIGCXX_MAJOR_VERSION == 3
 		return sigc::signal<R(V0,V1)>::operator()(a0, a1);
-#else
-		return sigc::signal2<R,V0,V1>::operator()(a0, a1);
-#endif
 	}
 };
 
 template <class R, class V0, class V1, class V2>
-#if SIGCXX_MAJOR_VERSION == 3
 class PSignal3: public PSignal, public sigc::signal<R(V0, V1, V2)>
-#else
-class PSignal3: public PSignal, public sigc::signal3<R,V0,V1,V2>
-#endif
 {
 public:
 	R operator()(V0 a0, V1 a1, V2 a2)
@@ -135,11 +101,7 @@ public:
 			callPython(pArgs);
 			Org_Py_DECREF(pArgs);
 		}
-#if SIGCXX_MAJOR_VERSION == 3
 		return sigc::signal<R(V0,V1,V2)>::operator()(a0, a1, a2);
-#else
-		return sigc::signal3<R,V0,V1,V2>::operator()(a0, a1, a2);
-#endif
 	}
 };
 
